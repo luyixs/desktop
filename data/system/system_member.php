9d5be50420ec6b02c15a923e2f61607d25491ace
luyixs <92432023+luyixs@users.noreply.github.com>
2022-10-09 01:16:32 +0800

    1

---
+++ data/system/system_member.php
@@ -0,0 +1,71 @@
+<?php exit;?>{
+    "1": {
+        "name": "admin",
+        "path": "admin",
+        "password": "0fd9911d0ce18b8f2c1b06a839db94b1",
+        "userID": "1",
+        "role": "1",
+        "config": {
+            "sizeMax": "0",
+            "sizeUse": 1024
+        },
+        "groupInfo": {
+            "1": "write"
+        },
+        "createTime": 1665245910,
+        "status": 1,
+        "lastLogin": 1665245920
+    },
+    "100": {
+        "userID": "100",
+        "name": "demo",
+        "password": "fe01ce2a7fbac8fafaed7c982a04e229",
+        "role": "2",
+        "config": {
+            "sizeMax": 5,
+            "sizeUse": 1048576
+        },
+        "groupInfo": {
+            "1": "write"
+        },
+        "path": "demo",
+        "status": 1,
+        "lastLogin": 1665247774,
+        "createTime": 1665245910
+    },
+    "101": {
+        "userID": "101",
+        "name": "guest",
+        "password": "084e0343a0486ff05530df6c705c8bb4",
+        "role": "100",
+        "config": {
+            "sizeMax": 0.1,
+            "sizeUse": 1048576
+        },
+        "groupInfo": {
+            "1": "read"
+        },
+        "path": "guest",
+        "status": 1,
+        "lastLogin": "",
+        "createTime": 1665245910
+    },
+    "102": {
+        "userID": "102",
+        "name": "luyixs",
+        "nickName": "luyixs",
+        "password": "6b6de437b9c414b704d019696ff8dde5",
+        "role": "1",
+        "config": {
+            "sizeMax": 2,
+            "sizeUse": 1048576
+        },
+        "groupInfo": {
+            "1": "write"
+        },
+        "path": "luyixs",
+        "status": 1,
+        "lastLogin": 1665247899,
+        "createTime": 1665246067
+    }
+}
